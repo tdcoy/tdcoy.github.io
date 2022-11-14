07ac3e56d6dd82421a0eb033d79c2e84242603ac
tdcoy510 <tdcoy510@gmail.com>
2022-11-14 17:44:21 -0500

    saved this time

---
+++ js/testing.js
@@ -0,0 +1,52 @@
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+      const loader = new GLTFLoader();
+
+      
+      // Our Javascript will go here.
+      const scene = new THREE.Scene();
+
+      //FOV (in degrees), aspect ratio, near and far clipping plane
+      const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+
+      const renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      document.body.appendChild(renderer.domElement);
+
+      //Render cube
+      //const geometry = new THREE.BoxGeometry(1, 1, 1);
+      //const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      //const cube = new THREE.Mesh(geometry, material);
+
+      loader.load(
+        "assets/models/skull.glb",
+        function (gltf) {
+          scene.add(gltf.scene);
+        },
+        undefined,
+        function (error) {
+          console.error(error);
+        }
+      );
+
+      //Add cube at pos 0,0,0 in scene
+      scene.add(cube);
+
+      //Set camera position
+      camera.position.z = 5;
+
+      //Render loop
+      function animate() {
+        requestAnimationFrame(animate);
+
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        renderer.render(scene, camera);
+      }
+
+      animate();
